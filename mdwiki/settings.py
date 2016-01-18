e211c5d885f068f2b070ee9b63b98a5750997d2d
Artemiy Solopov <art-solopov@yandex.ru>
2016-01-19 01:56:42 +0300

    Created the Wiki app and models

--- mdwiki/settings.py
+++ mdwiki/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wiki.apps.WikiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'mdwiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mdwiki'
     }
 }
 
---
+++ wiki/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Article(models.Model):
+    name = models.CharField('Article name', max_length=1100)
+    body = models.TextField('Article body in Markdown')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Alias(models.Model):
+    name = models.CharField('Alias', max_length=1100)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
