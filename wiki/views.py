ee3a42cf252df96b8559a15c5ce5ab6d2326419d
Artemiy Solopov <art-solopov@yandex.ru>
2016-01-21 00:58:37 +0300

    Changed function view to class view

--- wiki/views.py
+++ wiki/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_safe
+from django.views.generic import TemplateView
 from .models import Article
 
 
-@require_safe
-def index(request):
-    """Index view for the wiki
-    """
-    latest = Article.objects.order_by('-updated')[:5]
-    return render(request, 'wiki/index.html', {'latest': latest})
+class HomePageView(TemplateView):
+
+    """Home page view"""
+
+    template_name = 'wiki/index.html'
+    http_method_names = ['get', 'head']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_articles'] = Article.objects.order_by('-updated')[:5]
+        return context
+
