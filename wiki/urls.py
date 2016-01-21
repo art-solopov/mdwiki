148ebcd135edee94ffd2b5090cf366f953f88c7f
Artemiy Solopov <art-solopov@yandex.ru>
2016-01-22 00:39:18 +0300

    Detail view

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name='index')
+    url(r'^$', views.HomePageView.as_view(), name='index'),
+    url(r'^(?P<name>[\w\s]+)', views.ArticleDetailView.as_view(), name='article-detail')
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_safe
-from django.views.generic import TemplateView
+import markdown
+import bleach
+from django.views.generic import TemplateView, DetailView
+
 from .models import Article
 
 
@@ -16,3 +17,22 @@ class HomePageView(TemplateView):
         context['latest_articles'] = Article.objects.order_by('-updated')[:5]
         return context
 
+
+class ArticleDetailView(DetailView):
+
+    model = Article
+    slug_field = 'name'
+    slug_url_kwarg = 'name'
+    context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['compiled_body'] = bleach.clean(
+            markdown.markdown(
+                context['article'].body,
+                extensions=[
+                    'markdown.extensions.wikilinks'
+                ]
+            )
+        )
+        return context
