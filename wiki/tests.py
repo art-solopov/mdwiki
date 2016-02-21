be1bd0fb4d606f936069bd17e0c3a90ccde8a090
Artemiy Solopov <art-solopov@yandex.ru>
2016-02-21 23:20:21 +0300

    Testing & some template tinkering

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,5 +1,31 @@
+import textwrap
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+
+from .models import Article
+
+class ArticleTestCase(TestCase):
+
+    """Testing the Article model"""
+
+    def setUp(self):
+        Article.objects.create(
+            name='Test article',
+            body=textwrap.dedent('''\
+            ## This is some test markdown!
+
+            It has [[Wiki links]] and stuff.
+            ''')
+        )
+
+    def test_body_as_html(self):
+        article = Article.objects.get(name='Test article')
+        self.assertIn(
+            reverse('article-detail', kwargs={ 'name': 'Wiki links' }),
+            article.body_as_html()
+        )
+
 
 class HomePageViewTestCase(TestCase):
 
