c343379ca31551eba6366ee8d039d2445c94410d
Artemiy Solopov <art-solopov@yandex.ru>
2016-04-04 00:58:27 +0300

    Custom Bootstrap form tags

--- mdwiki/settings.py
+++ mdwiki/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'wiki.apps.WikiConfig',
+    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
