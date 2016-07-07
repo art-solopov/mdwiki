f2b090b01ee81bb73d8053e4bb9e9d2f94162bfb
Artemiy Solopov <art-solopov@yandex.ru>
2016-07-07 22:35:27 +0300

    Fixed tests

--- wiki/tests_unit_models.py
+++ wiki/tests_unit_models.py
@@ -2,18 +2,12 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 
-from .models import Article, Alias
-from .test_utils import NewArticleMixin, NewAliasMixin
+from wiki.models import Article, Alias
+from wiki.test_utils import NewArticleMixin, NewAliasMixin
 
 class ArticleTestCase(NewArticleMixin, TestCase):
 
     """Testing the Article model"""
-
-    def test_body_as_html(self):
-        self.assertIn(
-            reverse('article-detail', kwargs={ 'slug': 'wiki-links' }),
-            self.article.body_as_html()
-        )
 
 class AliasTestCase(NewAliasMixin, TestCase):
 
